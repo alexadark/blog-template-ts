e8a069474fbb0dec1eee63ccf895410e09061bfd
Alexa spalato <alexaspalato@gmail.com>
2023-06-22 10:03:12 +0200

    footer, header, responsive cards

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,14 +1,14 @@
 import { useLoaderData } from "@remix-run/react";
 import { render } from "storyblok-rich-text-react-renderer";
-import SocialShare from "./SocialShare";
 
 const Footer = () => {
   const { footerText } = useLoaderData();
   return (
-    <footer className="py-10">
+    <footer className="py-10 ">
       <div className="center-container">
-        <div className="text-center">{render(footerText)}</div>
-        <SocialShare />
+        <div>
+          {render(footerText)} &copy; {new Date().getFullYear()}
+        </div>
       </div>
     </footer>
   );
--- app/components/bloks/PostCard.tsx
+++ app/components/bloks/PostCard.tsx
@@ -7,25 +7,41 @@ const PostCard = ({ post }: PostStoryblok) => {
   const { headline, teaser, categories, image } = post.content;
 
   return (
-    <article className="mb-7 bg-dark-50 border border-dark-25  px-5 pt-14 pb-10 rounded-lg shadow-sm translate duration-500 hover:-translate-y-1 hover:shadow-primary relative">
+    <article className="mb-7 bg-dark-50 border border-dark-25  px-5 py-7 rounded-lg shadow-sm translate duration-500 hover:-translate-y-1 hover:shadow-primary relative">
       <Link to={`/${post.full_slug}`}>
-        <div className="mr-5 text-lg font-bold text-primary absolute top-3 left-5">
-          {format(new Date(post.created_at), "MMMM dd, yyyy")}
+        <div className="flex flex-wrap justify-between items-center gap-3 mb-4">
+          <div className="mr-5 text-lg font-bold text-primary">
+            {format(new Date(post.created_at), "MMMM dd, yyyy")}
+          </div>
+          <div className="">
+            <Categories categories={categories} />
+          </div>
         </div>
-        <div className="flex justify-between gap-5">
+        <div className="md:flex justify-between gap-5">
           {image && (
             <div className="flex items-center">
-              <img
-                src={`${image?.filename}/m/300x160/smart/filters:quality(60)/`}
-                alt={image?.alt}
-                className="rounded-lg"
-              />
+              <picture>
+                <source
+                  media="(min-width: 768px)"
+                  srcSet={`${image?.filename}/m/400x200/smart/filters:quality(60)/`}
+                />
+                <source
+                  media="(max-width: 767px)"
+                  srcSet={`${image?.filename}/m/767x380/smart/filters:quality(60)/`}
+                />
+                <source
+                  media="(max-width: 400px)"
+                  srcSet={`${image?.filename}/m/400x200/smart/filters:quality(60)/`}
+                />
+                <img
+                  src={`${image?.filename}/m/750x400/smart/filters:quality(60)/`}
+                  alt={image?.alt}
+                  className="rounded-lg"
+                />
+              </picture>
             </div>
           )}
           <div>
-            <div className="absolute right-5 top-5">
-              <Categories categories={categories} />
-            </div>
             <h2 className="font-bold text-2xl mb-2">{headline}</h2>
             <p>{teaser}</p>
           </div>
