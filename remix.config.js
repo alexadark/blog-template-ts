8e1a70a7f7e6134b2d1d2594596a176718f9033b
Alexa spalato <alexaspalato@gmail.com>
2023-10-16 18:09:09 +0200

    udpate remix 2

--- remix.config.js
+++ remix.config.js
@@ -1,24 +1,15 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ['**/.*'],
+  ignoredRouteFiles: ["**/.*"],
   tailwind: true,
   // When running locally in development mode, we use the built-in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.ts',
-  serverBuildPath: 'api/index.js',
-  serverModuleFormat: 'cjs',
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.ts",
+  serverBuildPath: "api/index.js",
+  serverModuleFormat: "cjs",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
-  serverDependenciesToBundle: ['react-syntax-highlighter'],
-
-  future: {
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-    v2_headers: true,
-    // unstable_dev: true,
-  },
-}
+  serverDependenciesToBundle: ["react-syntax-highlighter"],
+};
